3849d2c23497ae8fcb1c2c9ee76c265a6b44fab4
Pavel Keyno <pavel@mati.ru>
2015-07-24 03:37:22 +0300

    Added TXT2FR utility

--- FR2TXT.C
+++ FR2TXT.C
@@ -1,3 +1,22 @@
+/**
+ * @file   fr2txt.c
+ * @brief  Captain Comic II dialog converter from binary to ASCII-text
+ *
+ * Copyright (C) 2015 Pavel Keyno <src@keyno.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #define BUFFER_LEN 32000
 #define MAX_ELEM_COUNT 50
--- README.md
+++ README.md
@@ -5,5 +5,9 @@ This utilities created for editing some resource data of the game Captain Comic 
 This sources fully compatible with Borland C++ 3.1 (DOS).
 
 List of utilities: 
-fr2txt - converts binary storytext resource file to ASCII-text
+## fr2txt
+Converts binary storytext resource file to ASCII-text
+
+## txt2fr 
+Converts ASCII-text to binary storytext resource file. Each dialog must be separated by '--' (Without quotes).
 
